43e07e48320cd7e4b7dde23013750abb19a67318
Dolikansal <kansaldolly24@gmail.com>
2026-04-14 17:58:01 +0530

    fixed browser router import error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
 import Navbar from './components/nav';
 import Home from './components/home';
 import About from './components/about';
